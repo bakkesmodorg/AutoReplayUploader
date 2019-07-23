6701e828fcef1352e9a723d0cf0d28b622e14e18
Tyler Nichols <nichols.tyler@gmail.com>
2019-07-23 13:54:52 -0600

    Current hacked curl stuff

--- ConsoleUploader/main.cpp
+++ ConsoleUploader/main.cpp
@@ -3,6 +3,36 @@
 #include "Utils.h"
 #include "Replay.h"
 
+#include <fstream>
+#include <cstdlib>
+#include <cerrno>
+
+#ifdef _WIN32
+#pragma comment(lib, "Wldap32.Lib")
+#pragma comment(lib, "Crypt32.Lib")
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "libssl.lib")
+#pragma comment(lib, "Wldap32.lib")
+#pragma comment(lib, "Normaliz.lib")
+#pragma comment(lib, "libcurl_a.lib")
+#pragma comment(lib, "curlpp.lib")
+
+#include <windows.h>
+#include <Shlobj_core.h>
+#include <sstream>
+#include <memory>
+#include <curl/curl.h>
+
+//#include "curlpp/curlpp.cpp"
+#include "curlpp/cURLpp.hpp"
+#include "curlpp/Easy.hpp"
+#include "curlpp/Options.hpp"
+#include "curlpp/Exception.hpp"
+
+#include <thread>
+#include <future>
+#endif
+
 void Log(void* object, string message)
 {
 	cout << message << endl;
@@ -28,23 +58,93 @@ void BallchasingAuthTestComplete(void* object, bool result)
 	cout << "Ballchasing authtest completed with result: " << result;
 }
 
-int main()
+void PostFileCurl(string& url, string& filePath, string& paramName)
 {
-	string replayFile = "C:/Program Files (x86)/Steam/steamapps/common/rocketleague/Binaries/Win32/bakkesmod/data/autoupload.replay";
+	CURL *curl;
+	CURLcode res;
 
-	string exportDir = "C:/Program Files (x86)/Steam/steamapps/common/rocketleague/Binaries/Win32/bakkesmod/data/";
-	string replayName = "tyni";
+	curl_mime *form = NULL;
+	curl_mimepart *field = NULL;
+	struct curl_slist *headerlist = NULL;
+	static const char buf[] = "Expect:";
 
-	string replayPath = CalculateReplayPath(exportDir, replayName);
+	curl_global_init(CURL_GLOBAL_ALL);
 
-	Calculated* calculated = new Calculated("consoleuploader", "----boundary", &Log, &CalculatedUploadComplete, NULL);
-	Ballchasing* ballchasing = new Ballchasing("consoleuploader", "----boundary", &Log, &BallchasingUploadComplete, &BallchasingAuthTestComplete, NULL);
+	curl = curl_easy_init();
+	if (curl) {
+		/* Create the form */
+		form = curl_mime_init(curl);
 
-	*(ballchasing->authKey) = "";
-	*(ballchasing->visibility) = "public";
+		/* Fill in the file upload field */
+		field = curl_mime_addpart(form);
+		curl_mime_name(field, paramName.c_str());
+		curl_mime_filedata(field, filePath.c_str());
 
-	ballchasing->UploadReplay(replayFile);
-	calculated->UploadReplay(replayFile);
+		/* initialize custom header list */
+		headerlist = curl_slist_append(headerlist, buf);
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+		curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
 
-	system("PAUSE");
+		curl_easy_setopt(curl, CURLOPT_PROXY, "http://localhost:8888");
+
+		/* Perform the request, res will get the return code */
+		res = curl_easy_perform(curl);
+
+		/* Check for errors */
+		if (res != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				curl_easy_strerror(res));
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+
+		/* then cleanup the form */
+		curl_mime_free(form);
+
+		/* free slist */
+		curl_slist_free_all(headerlist);
+	}
+}
+
+void PostFileCurlcpp(string& url, string& filePath, string& paramName)
+{
+	try {
+		curlpp::Cleanup cleaner;
+		curlpp::Easy request;
+
+		request.setOpt(new curlpp::options::Verbose(true));
+		request.setOpt(new curlpp::options::Url(url));
+		
+		{
+			// Forms takes ownership of pointers!
+			curlpp::Forms formParts;
+			formParts.push_back(new curlpp::FormParts::Content("name1", "value1"));
+			formParts.push_back(new curlpp::FormParts::Content("name2", "value2"));
+
+			request.setOpt(new curlpp::options::HttpPost(formParts));
+		}
+
+		// The forms have been cloned and are valid for the request, even
+		// if the original forms are out of scope.
+		std::ofstream myfile(filePath);
+		myfile << request << std::endl << request << std::endl;
+	}
+	catch (curlpp::LogicError & e) {
+		std::cout << e.what() << std::endl;
+	}
+	catch (curlpp::RuntimeError & e) {
+		std::cout << e.what() << std::endl;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	string url("http://calculated.gg/api/upload");
+	string path("C:/Users/tyni/Desktop/979B723811E971FCE06E328BDF9F6172.replay");
+	string paramName("replays");
+
+	PostFileCurlcpp(url, path, paramName);
+
+	return EXIT_SUCCESS;
 }
--- Uploader/HttpClient.cpp
+++ Uploader/HttpClient.cpp
@@ -17,310 +17,7 @@ HttpClient::HttpClient(void)
 }
 
 
-std::future<std::string> checkupdate(std::string const& url, std::string const& body) {
-	return std::async(std::launch::async,
-		[](std::string const& url, std::string const& body) mutable {
-		try
-		{
 
-			std::list<std::string> header;
-			header.push_back("Content-Type: application/json");
-			header.push_back("User-Agent: BPM;2;" + std::to_string(0) + ";steam;" + std::to_string(0) + ";");
-			curlpp::Cleanup clean;
-			curlpp::Easy r;
-			r.setOpt(new curlpp::options::Url(url));
-			r.setOpt(new curlpp::options::HttpHeader(header));
-			if (body.size() > 0) {
-				r.setOpt(new curlpp::options::PostFields(body));
-				r.setOpt(new curlpp::options::PostFieldSize(body.length()));
-
-			}
-			std::ostringstream response;
-			r.setOpt(new curlpp::options::WriteStream(&response));
-
-			r.perform();
-
-			return std::string(response.str());
-		}
-
-		catch (curlpp::LogicError & e)
-		{
-			return std::string(e.what());
-		}
-
-		catch (curlpp::RuntimeError & e)
-		{
-			return std::string(e.what());// << std::endl;
-		}
-		catch (...)
-		{
-
-		}
-		return std::string("Err");
-	}, url, body);
-}
-
-HttpClient::~HttpClient(void)
-{
-	if (m_hConnectedEvent)
-		CloseHandle(m_hConnectedEvent);
-	if (m_hRequestOpenedEvent)
-		CloseHandle(m_hRequestOpenedEvent);
-	if (m_hRequestCompleteEvent)
-		CloseHandle(m_hRequestCompleteEvent);
-
-	Close();
-}
-
-void WINAPI HttpClient::Callback(HINTERNET hInternet,
-	DWORD dwContext,
-	DWORD dwInternetStatus,
-	LPVOID lpStatusInfo,
-	DWORD dwStatusInfoLen)
-{
-	SContext* pContext = (SContext*)dwContext;
-
-	switch (pContext->dwContext)
-	{
-	case CONTEXT_CONNECT:
-		if (dwInternetStatus == INTERNET_STATUS_HANDLE_CREATED)
-		{
-			INTERNET_ASYNC_RESULT *pRes = (INTERNET_ASYNC_RESULT *)lpStatusInfo;
-			pContext->pObj->m_hConnect = (HINTERNET)pRes->dwResult;
-			SetEvent(pContext->pObj->m_hConnectedEvent);
-		}
-		break;
-
-	case CONTEXT_REQUESTHANDLE: // Request handle
-	{
-		switch (dwInternetStatus)
-		{
-		case INTERNET_STATUS_HANDLE_CREATED:
-		{
-			INTERNET_ASYNC_RESULT *pRes = (INTERNET_ASYNC_RESULT *)lpStatusInfo;
-			pContext->pObj->m_hRequest = (HINTERNET)pRes->dwResult;
-			SetEvent(pContext->pObj->m_hRequestOpenedEvent);
-		}
-		break;
-
-		case INTERNET_STATUS_REQUEST_SENT:
-		{
-			DWORD *lpBytesSent = (DWORD*)lpStatusInfo;
-		}
-		break;
-
-		case INTERNET_STATUS_REQUEST_COMPLETE:
-		{
-			INTERNET_ASYNC_RESULT *pAsyncRes = (INTERNET_ASYNC_RESULT *)lpStatusInfo;
-			SetEvent(pContext->pObj->m_hRequestCompleteEvent);
-		}
-		break;
-
-		case INTERNET_STATUS_REDIRECT:
-			//string strRealAddr = (LPSTR) lpStatusInfo;
-			break;
-
-		case INTERNET_STATUS_RECEIVING_RESPONSE:
-			break;
-
-		case INTERNET_STATUS_RESPONSE_RECEIVED:
-		{
-			DWORD *dwBytesReceived = (DWORD*)lpStatusInfo;
-			//if (*dwBytesReceived == 0)
-			//    bAllDone = TRUE;
-
-		}
-		}
-	}
-	}
-}
-
-void HttpClient::Close()
-{
-	if (m_hInstance)
-	{
-		InternetCloseHandle(m_hInstance);
-		m_hInstance = NULL;
-	}
-
-	if (m_hConnect)
-	{
-		InternetCloseHandle(m_hConnect);
-		m_hConnect = NULL;
-	}
-
-	if (m_hRequest)
-	{
-		InternetCloseHandle(m_hRequest);
-		m_hRequest = NULL;
-	}
-}
-
-
-BOOL HttpClient::Connect(LPCTSTR lpszAddr, USHORT uPort, LPCTSTR lpszAgent, DWORD dwTimeOut)
-{
-	Close();
-
-	ResetEvent(m_hConnectedEvent);
-	ResetEvent(m_hRequestOpenedEvent);
-	ResetEvent(m_hRequestCompleteEvent);
-
-
-	if (!(m_hInstance = InternetOpen(lpszAgent,
-		INTERNET_OPEN_TYPE_PRECONFIG,
-		NULL,
-		NULL,
-		INTERNET_FLAG_ASYNC)))
-	{
-		return FALSE;
-	}
-
-
-	if (InternetSetStatusCallback(m_hInstance,
-		(INTERNET_STATUS_CALLBACK)&Callback)
-		== INTERNET_INVALID_STATUS_CALLBACK)
-	{
-		return FALSE;
-	}
-
-
-	m_context.dwContext = CONTEXT_CONNECT;
-	m_context.pObj = this;
-
-	m_hConnect = InternetConnect(m_hInstance,
-		lpszAddr,
-		uPort,
-		NULL,
-		NULL,
-		INTERNET_SERVICE_HTTP,
-		INTERNET_FLAG_KEEP_CONNECTION | INTERNET_FLAG_NO_CACHE_WRITE,
-		(DWORD)&m_context);
-
-
-	if (m_hConnect == NULL)
-	{
-		if (GetLastError() != ERROR_IO_PENDING)
-			return FALSE;
-
-		if (WaitForSingleObject(m_hConnectedEvent, dwTimeOut) == WAIT_TIMEOUT)
-			return FALSE;
-	}
-
-	if (m_hConnect == NULL)
-		return FALSE;
-
-
-	return TRUE;
-}
-
-BOOL HttpClient::Request(string method, string page, string headers, char* data, size_t data_size, DWORD flags, DWORD dwTimeOut)
-{
-
-	LPCTSTR szAcceptType = _T("*/*");
-
-
-	m_context.dwContext = CONTEXT_REQUESTHANDLE;
-	m_context.pObj = this;
-
-	m_hRequest = HttpOpenRequest(m_hConnect,
-		method.c_str(),
-		page.c_str(),
-		NULL,
-		NULL,
-		NULL,
-		INTERNET_FLAG_RELOAD | INTERNET_FLAG_KEEP_CONNECTION
-		| INTERNET_FLAG_NO_CACHE_WRITE | INTERNET_FLAG_FORMS_SUBMIT
-		| INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTPS
-		| INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTP | flags,
-		(DWORD)&m_context);
-
-
-	if (m_hRequest == NULL)
-	{
-		if (GetLastError() != ERROR_IO_PENDING)
-			return FALSE;
-
-		if (WaitForSingleObject(m_hRequestOpenedEvent, dwTimeOut) == WAIT_TIMEOUT)
-			return FALSE;
-	}
-
-
-	if (m_hRequest == NULL)
-		return FALSE;
-
-
-	HttpAddRequestHeaders(m_hRequest, headers.c_str(), headers.length(), HTTP_ADDREQ_FLAG_ADD);
-
-	string contentLength = "Content-Length" + to_string(data_size);
-	HttpAddRequestHeaders(m_hRequest, contentLength.c_str(), contentLength.length(), HTTP_ADDREQ_FLAG_ADD);
-
-	if (!HttpSendRequest(m_hRequest,
-		NULL,
-		0,
-		data,
-		data_size))
-	{
-		if (GetLastError() != ERROR_IO_PENDING)
-			return FALSE;
-
-	}
-
-
-	if (WaitForSingleObject(m_hRequestCompleteEvent, dwTimeOut) == WAIT_TIMEOUT)
-	{
-		Close();
-		return FALSE;
-	}
-
-
-	return TRUE;
-}
-
-DWORD HttpClient::Read(PBYTE pBuffer, DWORD dwSize, DWORD dwTimeOut)
-{
-	INTERNET_BUFFERS InetBuff;
-
-	FillMemory(&InetBuff, sizeof(InetBuff), 0);
-
-	InetBuff.dwStructSize = sizeof(InetBuff);
-	InetBuff.lpvBuffer = pBuffer;
-	InetBuff.dwBufferLength = dwSize - 1;
-
-
-	m_context.dwContext = CONTEXT_REQUESTHANDLE;
-	m_context.pObj = this;
-
-	if (!InternetReadFileEx(m_hRequest,
-		&InetBuff,
-		0,
-		(DWORD)&m_context))
-	{
-		if (GetLastError() == ERROR_IO_PENDING)
-		{
-			if (WaitForSingleObject(m_hRequestCompleteEvent, dwTimeOut) == WAIT_TIMEOUT)
-				return FALSE;
-		}
-		else
-			return FALSE;
-	}
-
-
-	return InetBuff.dwBufferLength;
-}
-
-DWORD HttpClient::GetStatusCode()
-{
-	DWORD statusCode = 0;
-	DWORD length = sizeof(DWORD);
-	HttpQueryInfo(
-		m_hRequest,
-		HTTP_QUERY_STATUS_CODE | HTTP_QUERY_FLAG_NUMBER,
-		&statusCode,
-		&length,
-		NULL
-	);
-	return statusCode;
-}
 
 
 DWORD WINAPI HttpPostThread(void* data) {
@@ -358,45 +55,6 @@ void HttpRequestAsync(HttpRequestObject* request)
 	http.detach();
 }
 
-vector<uint8_t> GetFileBytes(string filename)
-{
-	// open the file
-	std::streampos fileSize;
-	std::ifstream file(filename, ios::binary);
-
-	// get its size
-	file.seekg(0, ios::end);
-	fileSize = file.tellg();
-
-	if (fileSize <= 0) // in case the file does not exist for some reason
-	{
-		fileSize = 0;
-	}
-
-	// initialize byte vector to size of replay
-	vector<uint8_t> fileData(fileSize);
-
-	// read replay file from the beginning
-	file.seekg(0, ios::beg);
-	file.read((char*)&fileData[0], fileSize);
-	file.close();
-
-	return fileData;
-}
-
-string GetFileName(const string& s)
-{
-	char sep = '/';
-
-	size_t i = s.rfind(sep, s.length());
-	if (i != string::npos)
-	{
-		return(s.substr(i + 1, s.length() - i));
-	}
-
-	return("");
-}
-
 string AppendGetParams(string baseUrl, map<string, string> getParams)
 {
 	std::stringstream urlStream;
@@ -424,54 +82,56 @@ char* CopyToCharPtr(vector<uint8_t>& vector)
 	return reqData;
 }
 
-void HttpFileUploadAsync(string server, string path, string userAgent, string filepath, string paramName, string additionalHeaders, string uploadBoundary, int requestId, void* requester, void(*RequestComplete)(HttpRequestObject*))
+void HttpFileUpload(string server, string path, string filepath, string paramName, string additionalHeaders, int requestId, void* requester, void(*RequestComplete)(HttpRequestObject*))
 {
-	string filename = GetFileName(filepath);
+	
+}
 
-	// Get Replay file bytes to upload
-	auto bytes = GetFileBytes(filepath);
+void HttpFileUpload(string& url, string& filePath, string& paramName)
+{
+	CURL *curl;
+	CURLcode res;
 
-	// Construct headers
-	stringstream headers;
-	headers << "Content-Type: multipart/form-data;boundary=" << uploadBoundary;
-	if (!additionalHeaders.empty())
-		headers << "\r\n" << additionalHeaders;
-	auto header_str = headers.str();
+	curl_mime *form = NULL;
+	curl_mimepart *field = NULL;
+	struct curl_slist *headerlist = NULL;
+	static const char buf[] = "Expect:";
 
-	// Construct body
-	stringstream body;
-	body << "--" << uploadBoundary << "\r\n";
-	body << "Content-Disposition: form-data; name=\"" << paramName << "\"; filename=\"" << filename << "\"" << "\r\n";
-	body << "Content-Type: application/form-data" << "\r\n";
-	body << "\r\n";
-	body << string(bytes.begin(), bytes.end());
-	body << "\r\n";
-	body << "--" << uploadBoundary << "--" << "\r\n";
+	curl_global_init(CURL_GLOBAL_ALL);
 
-	// Convert body to vector of bytes instead of using str() which may have trouble with null termination chars
-	vector<uint8_t> buffer;
-	const string& str = body.str();
-	buffer.insert(buffer.end(), str.begin(), str.end());
+	curl = curl_easy_init();
+	if (curl) {
+		/* Create the form */
+		form = curl_mime_init(curl);
 
-	// Copy vector to char* for upload
-	char *reqData = CopyToCharPtr(buffer);
+		/* Fill in the file upload field */
+		field = curl_mime_addpart(form);
+		curl_mime_name(field, paramName.c_str());
+		curl_mime_filedata(field, filePath.c_str());
 
-	// Setup Http Request context
-	HttpRequestObject* ctx = new HttpRequestObject();
-	ctx->RequestId = requestId;
-	ctx->Requester = requester;
-	ctx->Headers = header_str;
-	ctx->Server = server;
-	ctx->Page = path;
-	ctx->Method = "POST";
-	ctx->UserAgent = userAgent;
-	ctx->Port = INTERNET_DEFAULT_HTTPS_PORT;
-	ctx->ReqData = reqData;
-	ctx->ReqDataSize = buffer.size();
-	ctx->RespData = new char[4096];
-	ctx->RespDataSize = 4096;
-	ctx->RequestComplete = RequestComplete;
-	ctx->Flags = INTERNET_FLAG_SECURE;
+		/* initialize custom header list */
+		headerlist = curl_slist_append(headerlist, buf);
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+		curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
 
-	HttpRequestAsync(ctx);
+		//curl_easy_setopt(curl, CURLOPT_PROXY, "http://localhost:8888");
+
+		/* Perform the request, res will get the return code */
+		res = curl_easy_perform(curl);
+
+		/* Check for errors */
+		if (res != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				curl_easy_strerror(res));
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+
+		/* then cleanup the form */
+		curl_mime_free(form);
+
+		/* free slist */
+		curl_slist_free_all(headerlist);
+	}
 }
